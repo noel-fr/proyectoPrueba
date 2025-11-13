9105309b18294fbb0d7e34b25a7d92e878e7869c
noel-fr <fnoel6050@gmail.com>
2025-11-13 11:20:18 -0600

    Commit inicial: Agrega .gitignore y código del proyecto

---
+++ ProyectoClaseQ4/DTOs/LoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoClaseQ4.DTOs;
+
+public class LoginDto
+{
+    [Required(ErrorMessage = "El email es requerido")]
+    [EmailAddress(ErrorMessage = "El email es invalido")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "El password es requerido")]
+    public string Password { get; set; } = string.Empty;
+
+}
