3da8d6b4d2d2ad753e8e5ddfb35cf3f241924936
noel-fr <fnoel6050@gmail.com>
2025-11-14 20:14:01 -0600

    Agregando Archivos

---
+++ ProyectoClaseQ4/Models/TaskItem.cs
@@ -0,0 +1,43 @@
+﻿using Google.Cloud.Firestore;
+namespace ProyectoClaseQ4.Models
+{
+    [FirestoreData]
+    public class TaskItem
+    {
+        [FirestoreProperty]
+        public string Id { get; set; } =  string.Empty;
+        
+        [FirestoreProperty]
+        public string Title { get; set; } = string.Empty;
+        
+        [FirestoreProperty]
+        public string Description { get; set; } = string.Empty;
+        
+        [FirestoreProperty]
+        public string AssignedToUserId { get; set; } = string.Empty;
+        
+        [FirestoreProperty]
+        public string CreatedByUserId { get; set; } = string.Empty;
+        
+        [FirestoreProperty]
+        public string CreatedByUserName { get; set; } = string.Empty;
+        
+        [FirestoreProperty]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        
+        [FirestoreProperty]
+        public DateTime? DueDate { get; set; }
+        
+        [FirestoreProperty]
+        public DateTime? CompletedAt { get; set; }
+
+        [FirestoreProperty] 
+        public bool IsCompleted { get; set; } = false;
+
+        [FirestoreProperty]
+        public string Priority { get; set; } = "medium";
+        
+        [FirestoreProperty]
+        public string Status { get; set; } = "pending";
+    }
+}
---
+++ ProyectoClaseQ4/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using Google.Cloud.Firestore;
+
+namespace ProyectoClaseQ4.Models
+{
+    [FirestoreData]
+    public class User
+    {
+        [FirestoreProperty]
+        public string Id { get; set; } = string.Empty;
+        
+        [FirestoreProperty]
+        public string Email { get; set; } = string.Empty;
+        
+        [FirestoreProperty]
+        public string Fullname { get; set; } = string.Empty;
+        
+        [FirestoreProperty]
+        public string Role { get; set; } = "user"; //"admin" o "user"
+        
+        [FirestoreProperty]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        
+        [FirestoreProperty]
+        public bool IsActive { get; set; } = true;
+    }
+}
