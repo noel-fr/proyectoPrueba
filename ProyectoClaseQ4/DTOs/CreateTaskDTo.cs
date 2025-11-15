d9fe28ebeab0214d1b46c344761cdc35c5019dcd
noel-fr <fnoel6050@gmail.com>
2025-11-14 19:02:37 -0600

    Agregando Archivos Actualizados

---
+++ ProyectoClaseQ4/DTOs/CreateTaskDTo.cs
@@ -0,0 +1,25 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace ProyectoClaseQ4.DTOs;
+
+public class CreateTaskDTo
+{
+    [Required (ErrorMessage ="El campo es requerido")]
+    [MaxLength(200,ErrorMessage ="El campo titulo no puede exceder los 1000 caracteres")]
+    public string Title { get; set; }
+    
+    [Required (ErrorMessage ="El campo es requerido")]
+    [MaxLength(1000,ErrorMessage ="El campo descripcion no puede exceder los 1000 caracteres")]
+    public string Description { get; set; }
+    
+    [Required (ErrorMessage ="El campo es requerido")]
+    public string AssignedToUserId { get; set; }
+    
+    public DateTime?  DueDate { get; set; }
+    
+    [Required (ErrorMessage ="El campo es requerido")]
+    [RegularExpression("(low|medium|high)$", ErrorMessage ="Prioridad invalida")]
+    public string Priority { get; set; }
+}
---
+++ ProyectoClaseQ4/DTOs/UpdateTaskDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ProyectoClaseQ4.DTOs;
+
+public class UpdateTaskDto
+{
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public string? AssignedToUserId { get; set; }
+    public DateTime?  DueDate { get; set; }
+    public string? Priority { get; set; }
+    public string? Status { get; set; }
+}
