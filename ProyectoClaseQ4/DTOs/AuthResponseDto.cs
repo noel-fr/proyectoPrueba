fa6d3cf7a839c605883cf7387971521ff01c4a87
noel-fr <fnoel6050@gmail.com>
2025-11-17 20:10:08 -0600

    Agregando Archivos

---
+++ ProyectoClaseQ4/DTOs/AuthResponseDto.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ProyectoClaseQ4.DTOs
+{
+    public class AuthResponseDto
+    {
+        public string Token { get; set; } = String.Empty;
+        public string UserId { get; set; } = String.Empty;
+        public string Email { get; set; } = String.Empty;
+        public string FullName { get; set; } = String.Empty;
+        public string Role { get; set; } = String.Empty;
+    }
+}
